28701ac1e9dd40ec9b5cce813152bdccb738758b
y3pat <y3patel@ucsd.edu>
2025-02-15 15:34:40 -0800

    step 1.2 lab 6 completed

--- meta/main.js
+++ meta/main.js
@@ -1,4 +1,5 @@
 let data = [];
+let commits = [];
 
 async function loadData() {
     data = await d3.csv('loc.csv', (row) => ({
@@ -9,8 +10,41 @@ async function loadData() {
         date: new Date(row.date + 'T00:00' + row.timezone),
         datetime: new Date(row.datetime),
     }));
-    console.log(data);
+    processCommits();
 }
+
+function processCommits() {
+    commits = d3.groups(data, (d) => d.commit)
+        .map(([commit, lines]) => {
+            let first = lines[0];
+
+            let { author, date, time, timezone, datetime } = first;
+
+            let ret = {
+                id: commit,
+                url: 'https://github.com/YOUR_REPO/commit/' + commit, 
+                author,
+                date,
+                time,
+                timezone,
+                datetime,
+                hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+                totalLines: lines.length,
+            };
+
+            Object.defineProperty(ret, 'lines', {
+                value: lines,
+                writable: false,
+                enumerable: false,
+                configurable: false 
+            });
+
+            return ret;
+        });
+
+    console.log("Processed Commits:", commits);
+}
+
 
 document.addEventListener('DOMContentLoaded', async () => {
     await loadData();
