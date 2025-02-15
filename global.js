f9d47a665212425fb918c3fcfe0463191b517e97
y3pat <y3patel@ucsd.edu>
2025-02-15 13:51:43 -0800

    Update global.js

--- global.js
+++ global.js
@@ -18,6 +18,7 @@ let pages = [
     { url: 'projects/', title: 'Projects' },
     { url: 'contact/', title: 'Contact' },
     { url: 'resume/', title: 'Resume' },
+    { url: 'meta/', title: 'Meta' },
     { url: 'https://github.com/y3pat', title: 'GitHub', external: true },
   ];
 
